6712d1d1f07fcbff201279049f823ba93d9d6b41
Barkshihun <barkshihun2@gmail.com>
2023-03-23 02:04:13 +0900

    오토리워드 최종

--- auto-reward/popup.js
+++ auto-reward/popup.js
@@ -1,24 +1,11 @@
 "use strict";
-const mobileBtn = document.body.querySelector("#mobile");
 const pcBtn = document.body.querySelector("#pc");
 const BASE = "https://www.bing.com/search?q=";
 const PC_TEXT = "1234567890123456789012345678901234";
-const MOBILT_TEXT = "qwertyuiopasdfghjklzxcvbnmqwer";
-mobileBtn.addEventListener("click", () => {
-  chrome.declarativeNetRequest.updateEnabledRulesets({ enableRulesetIds: ["mobile_rule"] }, () => {
-    for (let i = 0; i < 20; i++) {
-      const queryStr = MOBILT_TEXT.slice(i);
-      const url = BASE + queryStr;
-      window.open(url, undefined, "popup");
-    }
-  });
-});
 pcBtn.addEventListener("click", () => {
-  chrome.declarativeNetRequest.updateEnabledRulesets({ disableRulesetIds: ["mobile_rule"] }, () => {
-    for (let i = 0; i < 34; i++) {
-      const queryStr = PC_TEXT.slice(i);
-      const url = BASE + queryStr;
-      window.open(url, undefined, "popup");
-    }
-  });
+  for (let i = 0; i < 34; i++) {
+    const queryStr = PC_TEXT.slice(i);
+    const url = BASE + queryStr;
+    window.open(url, undefined, "popup");
+  }
 });
