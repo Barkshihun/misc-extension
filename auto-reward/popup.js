8ce07adf38e2bbe81c3bf9f79181e8a086f533ab
Barkshihun <barkshihun2@gmail.com>
2023-07-30 12:18:09 +0900

    오토리워드를 여러 개로 나눔

--- auto-reward/popup.js
+++ auto-reward/popup.js
@@ -1,11 +1,26 @@
 "use strict";
-const pcBtn = document.body.querySelector("#pc");
+const firstBtn = document.body.querySelector("#first");
+const secondBtn = document.body.querySelector("#second");
+const thirdBtn = document.body.querySelector("#third");
+const forthBtn = document.body.querySelector("#forth");
 const BASE = "https://www.bing.com/search?q=";
 const PC_TEXT = "1234567890123456789012345678901234";
-pcBtn.addEventListener("click", () => {
-  for (let i = 0; i < 34; i++) {
+const newPopup = (init, condition) => {
+  for (let i = init; i < condition; i++) {
     const queryStr = PC_TEXT.slice(i);
     const url = BASE + queryStr;
     window.open(url, undefined, "popup");
   }
+};
+firstBtn.addEventListener("click", () => {
+  newPopup(0, 8);
+});
+secondBtn.addEventListener("click", () => {
+  newPopup(8, 16);
+});
+thirdBtn.addEventListener("click", () => {
+  newPopup(16, 25);
+});
+forthBtn.addEventListener("click", () => {
+  newPopup(25, 34);
 });
