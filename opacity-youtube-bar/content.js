5538ed4e49660a086b796d8be6b32b6d51164b2a
Barkshihun <barkshihun2@gmail.com>
2023-07-30 12:21:25 +0900

    버튼으로 그때그때 유튜브 바 투명도 조절하게 변경

--- opacity-youtube-bar/content.js
+++ opacity-youtube-bar/content.js
@@ -1,23 +1,8 @@
 "use strict";
-const ytpProgressBarContainer = document.querySelector(".ytp-progress-bar-container");
-const ytpChromeControls = document.querySelector(".ytp-chrome-controls");
-const ytpPaidContentOverlayLink = document.querySelector(".ytp-paid-content-overlay-link");
 const setOpacity = (opacity) => {
-  ytpProgressBarContainer.style.opacity = opacity;
-  ytpChromeControls.style.opacity = opacity;
-  ytpPaidContentOverlayLink.style.opacity = opacity;
+  document.querySelector(".ytp-gradient-bottom").style.opacity = opacity;
+  document.querySelector(".ytp-chrome-bottom").style.opacity = opacity;
 };
-if (ytpProgressBarContainer) {
-  chrome.storage.local.get({ enabled: false, opacity: 0.1 }, ({ enabled, opacity }) => {
-    if (enabled === true) {
-      setOpacity(opacity);
-    }
-  });
-  chrome.runtime.onMessage.addListener(({ enabled, opacity }) => {
-    if (enabled) {
-      setOpacity(opacity);
-    } else {
-      setOpacity(1);
-    }
-  });
-}
+chrome.runtime.onMessage.addListener(({ opacity }) => {
+  setOpacity(opacity);
+});
--- opacity-youtube-bar/popup.js
+++ opacity-youtube-bar/popup.js
@@ -1,55 +1,21 @@
 "use strict";
-const input = document.querySelector("input");
-const indicator = document.querySelector("h1");
-const btn = document.querySelector("button");
+const btnZero = document.getElementById("zero");
+const btnHalf = document.getElementById("half");
+const btnOne = document.getElementById("one");
 
-const onEnabled = () => {
-  input.style.visibility = "visible";
-  btn.innerText = "활성화중";
-  chrome.action.setBadgeText({ text: "ON" });
-  chrome.action.setBadgeBackgroundColor({ color: [230, 0, 0, 255] });
-};
-const onDisabled = () => {
-  input.style.visibility = "hidden";
-  btn.innerText = "비활성화중";
-  chrome.action.setBadgeText({ text: "OFF" });
-  chrome.action.setBadgeBackgroundColor({ color: [0, 0, 255, 255] });
-};
-const sendQuery = (enabled, opacity) => {
+const sendQuery = (opacity) => {
   chrome.tabs.query({ active: true, currentWindow: true, url: "https://www.youtube.com/*" }, ([tab]) => {
     if (tab) {
-      chrome.tabs.sendMessage(tab.id, { enabled, opacity });
+      chrome.tabs.sendMessage(tab.id, { opacity });
     }
   });
 };
-const init = () => {
-  chrome.storage.local.get({ enabled: false, opacity: 0.1 }, ({ enabled, opacity }) => {
-    indicator.innerText = opacity;
-    input.value = opacity;
-    if (enabled) {
-      onEnabled();
-    } else {
-      onDisabled();
-    }
-  });
-};
-input.addEventListener("input", (event) => {
-  const opacity = event.target.value;
-  indicator.innerText = opacity;
-  chrome.storage.local.set({ opacity });
-  chrome.storage.local.get({ enabled: false }, ({ enabled }) => {
-    sendQuery(enabled, opacity);
-  });
+btnZero.addEventListener("click", () => {
+  sendQuery(0);
 });
-btn.addEventListener("click", () => {
-  chrome.storage.local.get({ enabled: false }, ({ enabled }) => {
-    if (enabled) {
-      onDisabled();
-    } else {
-      onEnabled();
-    }
-    sendQuery(!enabled, input.value);
-    chrome.storage.local.set({ enabled: !enabled });
-  });
+btnHalf.addEventListener("click", () => {
+  sendQuery(0.5);
 });
-init();
+btnOne.addEventListener("click", () => {
+  sendQuery(1);
+});
